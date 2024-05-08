2688046cb5f8a8b17fcb1b695509bd15f28003a8
Moyeon <135600903+Moyeon7@users.noreply.github.com>
2024-05-08 12:54:50 +0530

    Update subsets.c
    This code is now printing every step.

--- MADF/backtracking/subsets.c
+++ MADF/backtracking/subsets.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #define N 20
 
-int n, m;
+int n, m, count=0;
 
 void show(int w[N], int k, int x[N]){
+    printf("\n-------------------------------------------------------------");
     printf("\nx = ( ");
     for (int i = 1; i <= n; i++){
         if (i==n)
@@ -24,15 +25,46 @@ void show(int w[N], int k, int x[N]){
     }
     printf(" )\n");
 }
+void stsptr(int s, int k, int r, int w[], int x[], int i){
+    if(i==0){
+        printf("\nRoot Node: [%d,%d,%d]", s,k,r);
+        stsptr(s,k,r,w,x,i+1);
+    }
+    if(x[i]==1){
+        s=s+w[i]; k=k+1; r=r-w[i];
+        printf("\nLeft Node: [%d,%d,%d]", s,k,r);
+        stsptr(s, k, r, w, x, i+1);
+    }else if(x[i]==0){
+        k=k+1; r=r-w[i];
+        printf("\nRight Node: [%d,%d,%d]", s,k,r);
+        stsptr(s, k, r, w, x, i+1);
+    }else if(i==n){
+        if(x[i]==1){
+            s=s+w[i]; k=k+1; r=r-w[i];
+            printf("\nLeft Node: [%d,%d,%d]", s+w[i],k,r);
+            return;
+        }else{
+            k=k+1; r=r-w[i];
+            printf("\nRight Node: [%d,%d,%d]", s,k,r);
+            return;
+        }
+    }
+}
+
 void sumofsub(int s, int k, int r, int w[], int x[]){
     if (k > n)
         return;
-    x[k] = 1;
     
-    if (s + w[k] == m)
+    if (s + w[k] == m){
+        x[k]=1;
         show(w, k, x);
-    else if ((s + w[k] + w[k + 1]) <= m)
+        stsptr(0,1,r,w,x,0);
+    } 
+    else if ((s + w[k] + w[k + 1]) <= m){
+        x[k] = 1;
         sumofsub(s + w[k], k + 1, r, w, x);
+    }
+       
     if (((s + r - w[k]) >= m) && (s + w[k + 1] <= m)){
         x[k] = 0; 
         sumofsub(s, k + 1, r, w, x);
@@ -47,15 +79,13 @@ int main(int argc, char const *argv[]){
     int w[n + 1], x[n + 1]; 
 
     printf("Enter the elements\n");
-    for (int i = 1; i <= n; i++)
-    {
+    for (int i = 1; i <= n; i++){
         x[i] = 0;
         scanf("%d", &w[i]);
         r += w[i];
     }
     printf("Enter max weight: ");
     scanf("%d", &m);
-
     sumofsub(0, 1, r, w, x);
     return 0;
 }
@@ -75,5 +105,11 @@ int main(int argc, char const *argv[]){
 /*
 7
 27 22 17 6 3 2 1
+27
+*/
+
+/*
+5
+1 2 3 6 17
 27
 */
